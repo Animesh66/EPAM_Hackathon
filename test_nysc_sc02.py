c65d8cf518b8001752e53967784e62f4f4d8a889
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-01 15:31:55 +0530

    New code added

--- test_nysc_sc02.py
+++ test_nysc_sc02.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.nyse.com/")
 driver.maximize_window()
+
 driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
 #driver.find_element(By.ID, "page-search").send_keys("EPAM")
 driver.find_element(By.ID, "page-search").send_keys("EPAM" + Keys.ENTER)
